59bfb19a4bd8c60f8b93c4f6f28a69932c2592ee
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2018-07-12 12:44:46 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const verbs = require('./dict/verbs');
+const nouns = require('./dict/nouns');
+
+const nounsCount = nouns.length;
+const verbsCount = verbs.length;
+
+const negativeProbability = 0.25;
+
+say: () => {
+    if (nounsCount > 0 && verbsCount > 0) {
+        const one = Math.floor(Math.random() * verbsCount);
+        const two = Math.floor(Math.random() * nounsCount);
+        const three = Math.floor(Math.random() * nounsCount);
+
+        return ((Math.random() < negativeProbability) ? 'не ' : '') + verbs[one] + ' ' + nouns[two] + ' ' + nouns[three];
+    }
+
+    return '';
+}
+
+
+module.exports = say();
