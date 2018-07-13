55cecf3dfc9505de22d6b783f2cac796bd35d6e4
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2018-07-13 14:34:46 +0200

    typo fix

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const countSchemas = schemas.length;
 
 const say = function() {
     return schemas[Math.floor(Math.random() * countSchemas)]();
-    return '';
 }
 
 module.exports = say(); // string result of the function
