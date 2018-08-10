236ff9fbc39e3f7a01a7afce500d029dc6816987
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2018-08-10 12:15:42 +0200

    random is used in main also

--- phrase/main.js
+++ phrase/main.js
@@ -1,8 +1,8 @@
+const random = require('./tools').random;
 const schemas = require('./schemas');
-const countSchemas = schemas.length;
 
 const say = function() {
-    return schemas[Math.floor(Math.random() * countSchemas)]();
+    return schemas[random(schemas.length)]();
 }
 
 module.exports = say(); // string result of the function
